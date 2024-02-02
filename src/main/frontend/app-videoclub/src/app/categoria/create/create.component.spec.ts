5a6e01de9d1996d3cd7bf906e346e3a0f2861adb
Cristian-Serrano <gcristiag@gmail.com>
2024-02-02 10:56:03 +0100

    a

--- src/main/frontend/app-videoclub/src/app/categoria/create/create.component.spec.ts
+++ src/main/frontend/app-videoclub/src/app/categoria/create/create.component.spec.ts
@@ -11,7 +11,7 @@ describe('CreateComponent', () => {
       declarations: [ CreateComponent ]
     })
     .compileComponents();
-
++
     fixture = TestBed.createComponent(CreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
